3127bcf7f087c884f945c56e6514cd11daac5d22
sherlyc <sherlyc@gmail.com>
2017-09-24 20:13:29 +1300

    updated gitignore

--- proptypes-tweet/src/index.js
+++ proptypes-tweet/src/index.js
@@ -4,22 +4,47 @@ import "./index.css";
 import moment from "moment";
 import PropTypes from "prop-types";
 
-function Tweet({ tweet }) {
-  return (
-    <div className="tweet">
-      <Avatar hash={tweet.gravatar} />
-      <div className="content">
-        <NameWithHandle author={tweet.author} />
-        <Time time={tweet.timestamp} />
-        <Message text={tweet.message} />
-        <div className="buttons">
-          <ReplyButton />
-          <RetweetButton count={tweet.retweets} />
-          <LikeButton count={tweet.likes} />
-          <MoreOptionsButton />
-        </div>
-      </div>
-    </div>
+function Tweet({
+  tweet
+}) {
+  return ( <
+    div className = "tweet" >
+    <
+    Avatar hash = {
+      tweet.gravatar
+    }
+    /> <
+    div className = "content" >
+    <
+    NameWithHandle author = {
+      tweet.author
+    }
+    /> <
+    Time time = {
+      tweet.timestamp
+    }
+    /> <
+    Message text = {
+      tweet.message
+    }
+    /> <
+    div className = "buttons" >
+    <
+    ReplyButton / >
+    <
+    RetweetButton count = {
+      tweet.retweets
+    }
+    /> <
+    LikeButton count = {
+      tweet.likes
+    }
+    /> <
+    MoreOptionsButton / >
+    <
+    /div> <
+    /div> <
+    /div>
   );
 }
 
@@ -35,72 +60,107 @@ var testTweet = {
   timestamp: "2016-07-30 21:24:37"
 };
 
-function Avatar({ hash }) {
+function Avatar({
+  hash
+}) {
   var url = `https://www.gravatar.com/avatar/${hash}`;
-  return <img src={url} className="avatar" alt="avatar" />;
+  return <img src = {
+    url
+  }
+  className = "avatar"
+  alt = "avatar" / > ;
 }
 
-function Message({ text }) {
-  return <div className="message">{text}</div>;
+function Message({
+  text
+}) {
+  return <div className = "message" > {
+    text
+  } < /div>;
 }
 
-Message.propTypes = {
-  text: PropTypes.string
-}
-
-function NameWithHandle({ author }) {
-  const { name, handle } = author;
-  return (
-    <span className="name-with-handle">
-      <span className="name">{name}</span>
-      <span className="handle">@{handle}</span>
-    </span>
+function NameWithHandle({
+  author
+}) {
+  const {
+    name,
+    handle
+  } = author;
+  return ( <
+    span className = "name-with-handle" >
+    <
+    span className = "name" > {
+      name
+    } < /span> <
+    span className = "handle" > @ {
+      handle
+    } < /span> <
+    /span>
   );
 }
 
-const Time = ({ time }) => {
+const Time = ({
+  time
+}) => {
   const timeString = moment(time).fromNow();
-  return <span className="time">{timeString}</span>;
+  return <span className = "time" > {
+    timeString
+  } < /span>;
 };
 
-Time.propTypes = {
-  time: PropTypes.string
-}
+const ReplyButton = () => < i className = "fa fa-reply reply-button" / > ;
 
-const ReplyButton = () => <i className="fa fa-reply reply-button" />;
-
-function Count({ count }) {
+function Count({
+  count
+}) {
   if (count > 0) {
-    return <span className="retweet-count">{count}</span>;
+    return <span className = "retweet-count" > {
+      count
+    } < /span>;
   } else {
     return null;
   }
 }
 
-const RetweetButton = ({ count }) => (
-  <span className="retweet-button">
-    <i className="fa fa-retweet" />
-    <Count count={count} />
-  </span>
+const RetweetButton = ({
+  count
+}) => ( <
+  span className = "retweet-button" >
+  <
+  i className = "fa fa-retweet" / >
+  <
+  Count count = {
+    count
+  }
+  /> <
+  /span>
 );
 
 RetweetButton.propTypes = {
   count: PropTypes.number
 };
 
-const LikeButton = ({ count }) => (
-  <span className="like-button">
-    <i className="fa fa-heart" />
-    {count > 0 && <span className="like-count">{count}</span>}
-  </span>
-);
+const LikeButton = ({
+    count
+  }) => ( <
+    span className = "like-button" >
+    <
+    i className = "fa fa-heart" / > {
+      count > 0 && < span className = "like-count" > {
+        count
+      } < /span>} <
+      /span>
+    );
 
-LikeButton.propTypes = {
-  count: PropTypes.number
-};
+    LikeButton.propTypes = {
+      count: PropTypes.number
+    };
 
-const MoreOptionsButton = () => (
-  <i className="fa fa-ellipsis-h more-options-button" />
-);
+    const MoreOptionsButton = () => ( <
+      i className = "fa fa-ellipsis-h more-options-button" / >
+    );
 
-ReactDOM.render(<Tweet tweet={testTweet} />, document.querySelector("#root"));
+    ReactDOM.render( < Tweet tweet = {
+        testTweet
+      }
+      />, document.querySelector("#root"));
