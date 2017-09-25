e8c127d45ebc1ebdd36ac006b6ea39b7f09b7beb
sherlyc <sherlyc@gmail.com>
2017-09-25 17:00:58 +1300

    added recipientLabel and stamp

--- envelope/src/index.js
+++ envelope/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
-function AddressLabel({ mailingLabel }) {
+function Stamp() {
+  return <div className="stamp">Stamp</div>;
+}
+
+function AddressLabel({ mailingLabel, className }) {
   var { name, addressLine1, addressLine2 } = mailingLabel;
   return (
-    <div className="mailingLabel">
+    <div className={`mailingLabel ${className}`}>
       <div className="name">{name}</div>
       <div className="addressLine1">{addressLine1}</div>
       <div className="addressLine2">{addressLine2}</div>
@@ -21,13 +25,34 @@ AddressLabel.propTypes = {
   }).isRequired
 };
 
+function Envelope({ toPerson, fromPerson }) {
+  return (
+    <div className="envelope">
+      <AddressLabel className="to-label" mailingLabel={fromPerson} />
+      <AddressLabel className="from-label" mailingLabel={toPerson} />
+      <Stamp />
+    </div>
+  );
+}
+
+Envelope.propTypes = {
+  toPerson: PropTypes.object,
+  fromPerson: PropTypes.object
+};
+
 var returnLabel = {
   name: "Full Name",
   addressLine1: "123 Fake St.",
   addressLine2: "Boston, MA 02118"
 };
 
+var recipientLabel = {
+  name: "Mrs. Receiver",
+  addressLine1: "123 James St.",
+  addressLine2: "San Francisco, CA 94101"
+};
+
 ReactDOM.render(
-  <AddressLabel mailingLabel={returnLabel} />,
+  <Envelope toPerson={recipientLabel} fromPerson={returnLabel} />,
   document.getElementById("root")
 );
