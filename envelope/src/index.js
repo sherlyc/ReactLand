5138a0fbf26efb3f404839502809836b8509c298
sherlyc <sherlyc@gmail.com>
2017-09-25 15:37:26 +1300

    returnLabel rendered

--- envelope/src/index.js
+++ envelope/src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function AddressLabel({ mailingLabel }) {
+  var { name, addressLine1, addressLine2 } = mailingLabel;
+  return (
+    <div className="mailingLabel">
+      <div className="name">{name}</div>
+      <div className="addressLine1">{addressLine1}</div>
+      <div className="addressLine2">{addressLine2}</div>
+    </div>
+  );
+}
+
+AddressLabel.propTypes = {
+  mailingLabel: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    addressLine1: PropTypes.string.isRequired,
+    addressLine2: PropTypes.string.isRequired
+  }).isRequired
+};
+
+var returnLabel = {
+  name: "Full Name",
+  addressLine1: "123 Fake St.",
+  addressLine2: "Boston, MA 02118"
+};
+
+ReactDOM.render(
+  <AddressLabel mailingLabel={returnLabel} />,
+  document.getElementById("root")
+);
