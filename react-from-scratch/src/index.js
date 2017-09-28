302693afcc6ff25cdba2f63a83396ff53de84b42
sherlyc <sherlyc@gmail.com>
2017-09-28 16:11:37 +1300

    rendered first react component at react-from-scratch

--- react-from-scratch/src/index.js
+++ react-from-scratch/src/index.js
@@ -1,1 +1,7 @@
-console.log("Testing our bundle");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const container = document.getElementById("container");
+const myDiv = <div>My First React Component!</div>;
+
+ReactDOM.render(myDiv, container);
