80072249f935723416126706512715b3e5ae6f28
sherlyc <sherlyc@gmail.com>
2017-09-29 16:19:28 +1300

    added ProductList component and PropTypes to it

---
+++ shoe-store/src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductList(props) {
+  return <div>ProductList</div>;
+}
+
+ProductList.propTypes = {
+  products: React.PropTypes.array.isRequired
+};
+
+export default ProductList;
