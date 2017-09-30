4751242705964897c91d212acca54440b4f5dd8d
sherlyc <sherlyc@gmail.com>
2017-09-30 18:37:42 +1300

    brand name display test passed

--- shoe-store/src/components/ProductList.js
+++ shoe-store/src/components/ProductList.js
@@ -3,9 +3,17 @@ import React from "react";
 function ProductList(props) {
   return (
     <ul>
-      {props.products.map(product => <li key={product.id}>{product.name}</li>)}
+      {props.products.map(product => (
+        <li key={product.id}>
+          {product.name} ({product.brand})
+        </li>
+      ))}
     </ul>
   );
 }
+
+// ProductList.propTypes = {
+//   products: React.PropTypes.array.isRequired
+// };
 
 export default ProductList;
--- shoe-store/src/components/ProductList.test.js
+++ shoe-store/src/components/ProductList.test.js
@@ -4,13 +4,27 @@ import ProductList from "./ProductList";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
+const mockProducts = [
+  { id: 1, name: "Mock Product 1", brand: "MockBrandA" },
+  { id: 2, name: "Mock Product 2", brand: "MockBrandB" },
+  { id: 3, name: "Mock Product 3", brand: "MockBrandC" }
+];
 
 it("should render a list of products as an unordered list", () => {
-  const mockProducts = [
-    { id: 1, name: "Mock Product 1", brand: "MockBrandA" },
-    { id: 2, name: "Mock Product 2", brand: "MockBrandB" },
-    { id: 3, name: "Mock Product 3", brand: "MockBrandC" }
-  ];
   const wrapper = shallow(<ProductList products={mockProducts} />);
   expect(wrapper.find("li").length).toEqual(mockProducts.length); //3
+});
+
+it("should display the product name in each `<li>` element", () => {
+  const wrapper = shallow(<ProductList products={mockProducts} />);
+  const firstElement = wrapper.find("li").first();
+  //Check that the product name is contained somewhere in this element
+  expect(firstElement.contains(mockProducts[0].name)).toEqual(true);
+});
+
+it("should display the brand name in each `<li>` element", () => {
+  const wrapper = shallow(<ProductList products={mockProducts} />);
+  const firstElement = wrapper.find("li").first();
+  //check that the brand name is contained somewhere in this element
+  expect(firstElement.contains(mockProducts[0].brand)).toEqual(true);
 });
